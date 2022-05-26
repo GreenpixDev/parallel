2dcd7bfab7554b0706ad1234dbb3bfd9165d325a
GreenpixDev <kol-roman.green@mail.ru>
2022-05-26 20:34:33 +0700

    Добавил OpenCL

--- OpenMP/OpenMP.cpp
+++ OpenMP/OpenMP.cpp
@@ -1,4 +1,4 @@
-﻿//#pragma comment(lib, "Win32/OpenCL.lib")
+﻿#pragma comment(lib, "Win32/OpenCL.lib")
 
 #include <iostream>
 #include <vector>
@@ -8,9 +8,9 @@
 #include <omp.h>
 #include <intrin.h>
 #include "lodepng.h"
-//#include <CL/cl.hpp>
+#include <CL/cl.hpp>
 
-//#define __CL_ENABLE_EXCEPTIONS
+#define __CL_ENABLE_EXCEPTIONS
 
 std::string readFileToString(std::string fileName) {
     std::string file_content;
@@ -54,7 +54,14 @@ void sequentialAdd(
 
     long double beginTimestamp = omp_get_wtime();
     for (int i = 0; i < (int)inputImage1.size(); i++) {
-        outputImage[i] = std::min(255, inputImage1[i] + inputImage2[i]);
+        int sum = inputImage1[i] + inputImage2[i];
+
+        if (sum > 255) {
+            outputImage[i] = 255;
+        }
+        else {
+            outputImage[i] = sum;
+        }
     }
     long double endTimestamp = omp_get_wtime();
 
@@ -72,74 +79,66 @@ void openmpAdd(
     long double beginTimestamp = omp_get_wtime();
 #pragma omp parallel for
     for (int i = 0; i < (int)inputImage1.size(); i++) {
-        outputImage[i] = std::min(255, inputImage1[i] + inputImage2[i]);
+        int sum = inputImage1[i] + inputImage2[i];
+        
+        if (sum > 255) {
+            outputImage[i] = 255;
+        }
+        else {
+            outputImage[i] = sum;
+        }
     }
     long double endTimestamp = omp_get_wtime();
 
     std::cout << "OpenMP Add: " << (endTimestamp - beginTimestamp) * 1000 << " ms" << std::endl;
 }
 
-//void openclAdd(
-//    std::vector<unsigned char>& inputImage1,
-//    std::vector<unsigned char>& inputImage2,
-//    std::vector<unsigned char>& outputImage
-//) {
-//    outputImage.clear();
-//    outputImage.resize(inputImage1.size());
-//
-//    int* pInputVector1 = new int[inputImage1.size()];
-//    for (int i = 0; i < inputImage1.size(); i++) {
-//        pInputVector1[i] = inputImage1[i];
-//    }
-//    int* pInputVector2 = new int[inputImage2.size()];
-//    for (int i = 0; i < inputImage2.size(); i++) {
-//        pInputVector2[i] = inputImage2[i];
-//    }
-//    int* pOutputVector = new int[outputImage.size()];
-//
-//    std::vector<cl::Platform> platforms;
-//    cl::Platform::get(&platforms);
-//    cl::Platform platform = platforms[0];
-//
-//    std::vector<cl::Device> devices;
-//    platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
-//    cl::Device device = devices[0];
-//
-//    cl::Context context(device);
-//    cl::CommandQueue queue(context, device);
-//
-//    cl::Buffer clInputVector1 = cl::Buffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, inputImage1.size() * sizeof(int), pInputVector1);
-//    cl::Buffer clInputVector2 = cl::Buffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, inputImage2.size() * sizeof(int), pInputVector2);
-//    cl::Buffer clOutputVector = cl::Buffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, outputImage.size() * sizeof(int), pOutputVector);
-//
-//    std::string sourceCode = readFileToString("kernel.cl");
-//
-//    cl::Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length() + 1));
-//    cl::Program program = cl::Program(context, source);
-//    program.build(devices);
-//    cl::Kernel kernel(program, "main");
-//
-//    int iArg = 0;
-//    kernel.setArg(iArg++, clInputVector1);
-//    kernel.setArg(iArg++, clInputVector2);
-//    kernel.setArg(iArg++, clOutputVector);
-//
-//    long double beginTimestamp = omp_get_wtime();
-//    queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(outputImage.size()), cl::NDRange(128));
-//    queue.finish();
-//    long double endTimestamp = omp_get_wtime();
-//
-//    std::cout << "OpenCL Add: " << (endTimestamp - beginTimestamp) * 1000 << " ms" << std::endl;
-//
-//    queue.enqueueReadBuffer(clOutputVector, CL_TRUE, 0, outputImage.size() * sizeof(int), pOutputVector);
-//    for (int i = 0; i < outputImage.size(); i++) {
-//        outputImage[i] = (unsigned char)pOutputVector[i];
-//    }
-//
-//    delete[] pInputVector1;
-//    delete[] pInputVector2;
-//    delete[] pOutputVector;
-//}
+void openclAdd(
+    std::vector<unsigned char>& inputImage1,
+    std::vector<unsigned char>& inputImage2,
+    std::vector<unsigned char>& outputImage
+) {
+    outputImage.clear();
+    outputImage.resize(inputImage1.size());
+
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+    cl::Platform platform = platforms[0];
+
+    std::vector<cl::Device> devices;
+    platform.getDevices(CL_DEVICE_TYPE_GPU, &devices);
+    cl::Device device = devices[0];
+
+    cl::Context context(device);
+    cl::CommandQueue queue(context, device);
+
+    cl::Buffer clInputVector1 = cl::Buffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, inputImage1.size() * sizeof(unsigned char), &inputImage1[0]);
+    cl::Buffer clInputVector2 = cl::Buffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, inputImage2.size() * sizeof(unsigned char), &inputImage2[0]);
+    cl::Buffer clOutputVector = cl::Buffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, outputImage.size() * sizeof(unsigned char), &outputImage[0]);
+
+    std::string sourceCode = readFileToString("kernel.cl");
+
+    cl::Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length() + 1));
+    cl::Program program = cl::Program(context, source);
+    program.build(devices);
+    cl::Kernel kernel(program, "main");
+
+    int iArg = 0;
+    kernel.setArg(iArg++, clInputVector1);
+    kernel.setArg(iArg++, clInputVector2);
+    kernel.setArg(iArg++, clOutputVector);
+    kernel.setArg(iArg++, outputImage.size());
+
+    queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(outputImage.size()), cl::NDRange(100));
+
+    long double beginTimestamp = omp_get_wtime();
+    queue.finish();
+    long double endTimestamp = omp_get_wtime();
+
+    std::cout << "OpenCL Add: " << (endTimestamp - beginTimestamp) * 1000 << " ms" << std::endl;
+
+    queue.enqueueReadBuffer(clOutputVector, CL_TRUE, 0, outputImage.size() * sizeof(unsigned char), &outputImage[0]);
+}
 
 void sequentialVectorizationAdd(
     std::vector<unsigned char>& inputImage1,
@@ -207,9 +206,9 @@ int main() {
     openmpAdd(image1, image2, outputImage);
     writeImage(outputImage, outputFilename, width, height);
 
-    /*outputFilename = "output/opencl add.png";
+    outputFilename = "output/opencl add.png";
     openclAdd(image1, image2, outputImage);
-    writeImage(outputImage, outputFilename, width, height);*/
+    writeImage(outputImage, outputFilename, width, height);
 
     outputFilename = "output/sequential vectorization add.png";
     sequentialVectorizationAdd(image1, image2, outputImage);
--- OpenMP/kernel.cl
+++ OpenMP/kernel.cl
@@ -1,7 +1,7 @@
 __kernel void main(
-	__global const int* pInputVector1,
-	__global const int* pInputVector2,
-	__global int* pOutputVectorHost,
+	__global const unsigned char* pInputVector1,
+	__global const unsigned char* pInputVector2,
+	__global unsigned char* pOutputVectorHost,
 	int elementsNumber
 ) {
 	int iJob = get_global_id(0);
